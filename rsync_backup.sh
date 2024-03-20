b6e1705c4a73e1d98ada02fbaf953b218b1f89ef
Ubuntu <ubuntu@ip-172-31-12-34.eu-west-1.compute.internal>
2024-03-20 21:39:15 +0000

    correction du script rsync_backup

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -2,17 +2,16 @@
 
 sauvegardeMariaDB() {
 
-	mysql -u admin --password 'Admin-P@ss' exercice1 > ~/backups/mariadb_backup/exercice1_backup.SQL
-	tar -czvf ~/backups/mariadb_backup/exercice1_backup.tar.gz ~/mariadb_backup/exercice1_backup.SQL
-	sshpass -p "vagrant" rsync -av --delete -e ssh ~/backups/mariadb_backup/exercice1_backup.tar.gz vagrant@192.168.10.11:/home/vagrant/backup/mariadb_backup/
-	#rsync -av -e ssh --delete ~/exercice1_backup.SQL/ vagrant@192.168.10.11:/home/vagrant/backup/mariadb_backup/
+	mysqldump -u admin -pAdmin-P@ss exercice1 > ~/backups/mariadb_backup/exercice1_backup.sql
+	tar -czvf ~/backups/mariadb_backup/exercice1_backup.tar.gz ~/backups/mariadb_backup/exercice1_backup.sql
+	rsync -Pav --delete -e "ssh -i /home/ubuntu/datascientest_architecture/.vagrant/machines/client1/docker/private_key" ~/backups/mariadb_backup/exercice1_backup.tar.gz vagrant@192.168.10.11:/home/vagrant/backup/mariadb_backup/
 	rm -rf ~/backups/mariadb_backup/*.tar.*
 
 }
 
 sauvegardeSite() {
 	tar -czvf ~/backups/site_backup/drupal_backup.tar.gz /var/www/html/drupal/
-        sshpass -p "vagrant" rsync -av --delete -e ssh ~/backups/site_backup/drupal_backup.tar.gz vagrant@192.168.10.11:/home/vagrant/backup/drupal_backup/
+        rsync -Pav --delete -e "ssh -i /home/ubuntu/datascientest_architecture/.vagrant/machines/client1/docker/private_key" ~/backups/site_backup/drupal_backup.tar.gz vagrant@192.168.10.11:/home/vagrant/backup/drupal_backup/
         rm -rf ~/backups/site_backup/*.tar.*
 }
 
